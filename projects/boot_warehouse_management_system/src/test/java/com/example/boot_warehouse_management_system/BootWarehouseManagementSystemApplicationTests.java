62c47af0c844fbe2805bb9c9235e4a0f75491fc6
jiangbi <18062102798@163.com>
2023-06-26 15:41:41 +0800

    [dev] 新增项目模块

---
+++ projects/boot_warehouse_management_system/src/test/java/com/example/boot_warehouse_management_system/BootWarehouseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boot_warehouse_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootWarehouseManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
